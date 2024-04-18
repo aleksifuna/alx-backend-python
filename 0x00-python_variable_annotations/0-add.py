57e2df58866c03b47bf0c6c91d33b62873e94e16
Alex Sifuna <aleksifuna@gmail.com>
2024-04-18 22:28:11 +0300

    added an annotated function that takes computes the sum of two variables

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contains one function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Computes and returns the sum of a and b"""
+    return a + b
---
+++ README.md
@@ -0,0 +1,1 @@
+This Repository contains backend programming projects done in python.
