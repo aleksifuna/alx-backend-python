ba0f8f855157154587d3e4bf90a6248d347573f4
Alex Sifuna <aleksifuna@gmail.com>
2024-04-18 22:46:50 +0300

    added a function that take a variable and returns its floor value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module contains one function floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Computes and return the floor of a float"""
+    return math.floor(n)
