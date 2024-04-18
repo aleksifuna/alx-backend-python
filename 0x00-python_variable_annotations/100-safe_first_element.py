fafd522c58fd8e394309d159b72b9cd888f7ccfd
Alex Sifuna <aleksifuna@gmail.com>
2024-04-19 01:02:10 +0300

    added an augmented code with duck-typed annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Suppies one function safe_first_element"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
