b618e0770f6a5c66453f45e97858317879367152
= <aleksifuna@gmail.com>
2024-04-22 22:37:35 +0000

    added a function that calls task_wait_random

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,5 +7,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """calls task_wait_random"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    return [await task for task in asyncio.as_completed(tasks)]
